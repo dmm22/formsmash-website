03e3fae64fcaa732070717d75cb7605f83422cf8
dmm22 <davidmartin71090@gmail.com>
2026-07-02 17:47:58 -0400

    Update styles

--- src/components/GifTextSection.tsx
+++ src/components/GifTextSection.tsx
@@ -7,19 +7,20 @@ type GifTextSectionProps = {
 }
 
 export default function GifTextSection({ gif, gifAlt, title, bullets, isAccentBackground = false }: GifTextSectionProps) {
-  const baseClasses = "grid grid-cols-[1fr_1fr] gap-24 place-items-center h-screen"
+  const baseClasses =
+    "flex flex-col gap-18 justify-center items-center h-[80dvh] lg:grid lg:grid-cols-[1fr_1fr] lg:justify-items-center lg:h-screen p-4"
 
   return (
     <section className={`${baseClasses} ${isAccentBackground ? "bg-accent" : ""}`}>
-      <img src={gif} alt={gifAlt} className="rounded-lg" />
       <div className="flex flex-col gap-14">
-        {title}
+        <h2>{title}</h2>
         <ul className="list-disc list-inside flex flex-col gap-6 text-xl">
           {bullets.map(bullet => (
             <li key={bullet}>{bullet}</li>
           ))}
         </ul>
       </div>
+      <img src={gif} alt={gifAlt} className="rounded-lg " />
     </section>
   )
 }
